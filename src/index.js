9fb16eb071a5bc1c364664ff6aded8d2cf4e1330
Mikalai Alistrau <alistrovn@gmail.com>
2022-08-05 17:53:39 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pairBrackets = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let pair = bracketsConfig[i][0] + bracketsConfig[i][1];
+    pairBrackets.push(pair);
+  }
+  let isPairExist = false;
+  let currentStr = str;
+  while (currentStr.length > 0) {
+    for (let i = 0; i < pairBrackets.length; i++) {
+      if (currentStr.indexOf(pairBrackets[i]) !== -1) {
+        currentStr = currentStr.replaceAll(pairBrackets[i], '');
+        isPairExist = true;
+      }
+    }
+    if (!isPairExist) {
+      return false;
+    } else {
+      isPairExist = false;
+    }
+  }
+  return true;
 }
